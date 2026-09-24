b7d8a046038cf6ac9a611ebb51d2c0fba7929920
Youssef-Ounana <youssefjocker74@gmail.com>
2026-09-24 13:49:36 +0200

    fix: prisma config use env DATABASE_URL

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   datasource: {
-    url: 'postgresql://edumanager_user:edumanager_pass_2026@localhost:5432/edumanager_db',
+    url: process.env.DATABASE_URL,
   },
 })
